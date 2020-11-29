df7d00ed31b961de22bc88c8523d96610ca87359
Erika <a01745158@itesm.mx>
2020-11-28 19:32:54 -0600

    Create inventario.h
    Avance en inventario

---
+++ inventario.h
@@ -0,0 +1,33 @@
+#include "producto.h"
+#ifndef INVENTARIO_C
+#define INVENTARIO_C
+
+#include <iostream>
+#include <string>
+using  namespace std;
+
+class Inventario{
+    int cantidad_producto;
+    string color_producto, tipo_prenda, diseno_producto;
+    Producto producto;
+
+string clasificar_producto(){
+}//Clasificar producto
+
+string mostrar_disponible(){
+}//mostrar disponible
+
+string borrar_producto(){   
+}//borrar producto
+
+string agregar_producto(){
+}//agregar producto
+
+string estimar_entrega(){   
+}//estimar entrega
+
+string generar_alerta_nueva_mercancia(){
+}//Alerta nueva mercancia (generar)
+
+};//Clase Inventario
+#endif
