912e88a012845b091c7555fc6e67b6da4d24514b
Erika <a01745158@itesm.mx>
2020-11-30 23:26:08 -0600

    Más avances

--- prueba.cpp
+++ prueba.cpp
@@ -2,20 +2,22 @@
 #include "inventario.h"
 #include "cliente.h"
 #include "producto.h"
-
+#include "dueno.h"
 #include <iostream>
 #include <string>
 
 using  namespace std;
 
 int main(){
-    Inventario inv1;
+    //Inventario inv1;
+    Dueno d1;
     cout<<"Pruebas generales"<<endl;
-    cout << inv1.agregar_producto() << endl;
-    inv1.mostrar_atributos_producto();
-    cout << inv1.mostrar_disponible() << endl; 
-    cout << inv1.agregar_Clientes() << endl;
-    inv1.mostrar_atributos_cliente();
-    inv1.borrar_producto();
-    inv1.recompensa_tipo_cliente();
+    //cout << inv1.agregar_producto() << endl;
+    //inv1.mostrar_atributos_producto();
+    //cout << inv1.mostrar_disponible() << endl; 
+    //cout << inv1.agregar_Clientes() << endl;
+    //inv1.mostrar_atributos_cliente();
+    //inv1.borrar_producto();
+    //inv1.recompensa_tipo_cliente();
+    d1.registrar_datos_dueno();
 };
