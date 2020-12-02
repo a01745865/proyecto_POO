8c529253a813abb74f7d39e5299b67eb662384ae
Erika <a01745158@itesm.mx>
2020-12-01 22:23:47 -0600

    Cambios para incluir mis pruebas

--- prueba.cpp
+++ prueba.cpp
@@ -17,14 +17,12 @@ int main(){
     inv1.borrar_producto();
     inv1.recompensa_tipo_cliente();
     inv1.hacer_pedido();//Esto es parte de la implementacion de la programacion individual de Angel
-    //Parte individual hecha por Erika
     Dueno d1;
     cout<<"Pruebas generales"<<endl;
-    d1.registrar_datos_dueno();
-    cout<<d1.print_info_dueno()<<endl;
-    d1.registrar_prestamo();
-    d1.registrar_pago_prestamo();
-    d1.mostrar_deuda();
-    d1.agregar_producto_inventario();
-    d1.calcular_ganancia();
+    d1.registrar_datos_dueno();//Parte individual hecha por Erika
+    cout<<d1.print_info_dueno()<<endl;//Parte individual hecha por Erika
+    d1.registrar_prestamo();//Parte individual hecha por Erika
+    d1.registrar_pago_prestamo();//Parte individual hecha por Erika
+    d1.mostrar_deuda();//Parte individual hecha por Erika
+
 };
