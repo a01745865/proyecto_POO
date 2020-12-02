8725da3e5da383fa1e4788c85c831ed81e157ef7
Erika <a01745158@itesm.mx>
2020-12-01 22:52:39 -0600

    Cambios en prueba pt2000

--- prueba.cpp
+++ prueba.cpp
@@ -7,7 +7,7 @@
 using  namespace std;
 
 int main(){
-
+    cout<<"Pruebas generales"<<endl;
     Inventario inv1;
     cout << inv1.agregar_producto() << endl;
     inv1.mostrar_atributos_producto();
@@ -17,8 +17,7 @@ int main(){
     inv1.borrar_producto();
     inv1.recompensa_tipo_cliente();
     inv1.hacer_pedido();//Esto es parte de la implementacion de la programacion individual de Angel
-    Dueno d1;
-    cout<<"Pruebas generales"<<endl;
+    Dueno d1;//Parte individual hecha por Erika
     d1.registrar_datos_dueno();//Parte individual hecha por Erika
     cout<<d1.print_info_dueno()<<endl;//Parte individual hecha por Erika
     d1.registrar_prestamo();//Parte individual hecha por Erika
